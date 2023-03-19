341a0c6015ac949513bdae0e3ba703d7acecad33
Levi0727 <molnar.levente11@gmail.com>
2023-03-19 17:33:11 +0100

    Add Troop movement

--- Polytopia Clone/Assets/Scripts/Map/View/Tile.cs
+++ Polytopia Clone/Assets/Scripts/Map/View/Tile.cs
@@ -9,6 +9,8 @@ public class Tile : MonoBehaviour
     public Color StartColor;
     [SerializeField] private Color hoverColor;
     [SerializeField] private Color neighborColor;
+    [SerializeField] private Color selectColor;
+    public Color SelectColor { get => selectColor; private set => selectColor = value; }
 
     private void Awake()
     {
@@ -24,6 +26,11 @@ public class Tile : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (TroopManager.Instance.SelectedTroop?.Tiles.Contains(this) ?? false)
+        {
+            GetComponent<Renderer>().material.color = hoverColor;
+            return;
+        }
         if (EventSystem.current.IsPointerOverGameObject())
         {
             Deselect();
@@ -31,28 +38,20 @@ public class Tile : MonoBehaviour
         }
 
         GetComponent<Renderer>().material.color = hoverColor;
-        HighlightNeighbors();
     }
 
-    private void OnMouseExit() =>
+    private void OnMouseExit()
+    {
+        if (TroopManager.Instance.SelectedTroop?.Tiles.Contains(this) ?? false)
+        {
+            GetComponent<Renderer>().material.color = selectColor;
+            return;
+        }
         Deselect();
+    }
 
     private void OnMouseDown() =>
         Select();
-
-    private void HighlightNeighbors()
-    {
-        foreach (Tile neighbor in Neighbors)
-            if (Controller.MapManager.Instance.SelectedTile?.gameObject != neighbor.gameObject)
-                neighbor.GetComponent<Renderer>().material.color = neighbor.GetComponent<Renderer>().material.color + neighborColor;
-    }
-
-    private void UnhighlightNeighbors()
-    {
-        foreach (Tile neighbor in Neighbors)
-            if (Controller.MapManager.Instance.SelectedTile?.gameObject != neighbor.gameObject)
-                neighbor.GetComponent<Renderer>().material.color = neighbor.GetComponent<Tile>().StartColor;
-    }
 
     public void Select()
     {
@@ -61,20 +60,16 @@ public class Tile : MonoBehaviour
             Deselect();
             return;
         }
-
         if (Controller.MapManager.Instance.SelectedTile == this)
         {
             Controller.MapManager.Instance.SelectedTile = null;
             return;
         }
-
         Controller.MapManager.Instance.SelectedTile = this;
     }
 
     public void Deselect()
     {
-        UnhighlightNeighbors();
-
         if (Controller.MapManager.Instance.SelectedTile != this)
             GetComponent<Renderer>().material.color = StartColor;
     }
