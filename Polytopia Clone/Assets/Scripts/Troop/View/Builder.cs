0cfcf24a0b1715002bd315ef61e977cb141fb766
Levi0727 <molnar.levente11@gmail.com>
2023-03-22 20:57:30 +0100

    Add Troops

---
+++ Polytopia Clone/Assets/Scripts/Troop/View/Builder.cs
@@ -0,0 +1,16 @@
+namespace View
+{
+    public class Builder : TroopBase
+    {
+        public override Model.TroopBase ToModel(Model.Player player)
+        {
+            Model.TroopBase builder = new Model.Builder();
+            builder.OnDamageTaken += TakeDamage;
+            builder.TroopProperty = new Model.TroopProperty(
+                troopProperties.Health, troopProperties.Damage,
+                troopProperties.MovementRange, troopProperties.AttackRange);
+            builder.Cost = new Model.Cost(cost.MoneyCost, cost.MaterialCost, cost.FoodCost);
+            return builder;
+        }
+    }
+}
