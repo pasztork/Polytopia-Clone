133953ac0ee01222171a4a1d66b61d29a4e80e73
Levi0727 <molnar.levente11@gmail.com>
2023-04-09 18:12:14 +0200

    Fix Boat attack

--- Polytopia Clone/Assets/Scripts/Model/Troop/Troops/OffensiveWaterTroop.cs
+++ Polytopia Clone/Assets/Scripts/Model/Troop/Troops/OffensiveWaterTroop.cs
@@ -1,7 +1,45 @@
-﻿namespace Model
+﻿using System.Collections.Generic;
+
+namespace Model
 {
     public abstract class OffensiveWaterTroop : WaterTroop
     {
+        protected bool attackedInTurn;
+
+        public OffensiveWaterTroop() : base()
+        {
+            GameManager.Get<TurnManagerBase>().OnTurnStarted +=
+                (player) => attackedInTurn = false;
+        }
+
+        public override List<TileBase> Attack(TroopBase troop)
+        {
+            if (!TilesInAttackRange.Contains(troop.Tile) || attackedInTurn)
+                return null;
+
+            attackedInTurn = true;
+            bool damageTaken = troop.TakeDamage(TroopProperty.Damage);
+            if (!damageTaken)
+            {
+                troop.Player.RaiseOnAttackMissed(this, troop);
+                return null;
+            }
+
+            return new List<TileBase>() { troop.Tile, troop.Tile };
+        }
+
+        public override List<TileBase> Attack(BuildingBase building)
+        {
+            IList<TileBase> tilesInRange = TilesInAttackRange;
+            tilesInRange.Add(Tile);
+            if (!tilesInRange.Contains(building.Tile) || attackedInTurn)
+                return null;
+
+            attackedInTurn = true;
+            building.TakeDamage(TroopProperty.Damage);
+            return new List<TileBase>() { building.Tile };
+        }
+
         public override void ApplyAllPropertyBonus(Player player)
         {
             base.ApplyAllPropertyBonus(player);
