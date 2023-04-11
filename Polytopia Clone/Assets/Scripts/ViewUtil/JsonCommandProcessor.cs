b3a673d5da539c521dff29f1104e4f3b9818e7cc
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-04-11 16:33:56 +0300

    feat: add command processor

---
+++ Polytopia Clone/Assets/Scripts/ViewUtil/JsonCommandProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ViewUtil
+{
+    public class JsonCommandProcessor
+    {
+        private LogView.JsonActionObject _command = null;
+
+        public IDictionary<string, Func<bool>> Actions { get; } = new Dictionary<string, Func<bool>>();
+
+        public bool Process(string commandString)
+        {
+            _command = JsonSerializer.Deserialize<LogView.JsonActionObject>(commandString);
+            return VerifyCommandExists() && Actions[_command.Action]();
+        }
+
+        private bool VerifyCommandExists() => Actions.ContainsKey(_command.Action);
+    }
+}
