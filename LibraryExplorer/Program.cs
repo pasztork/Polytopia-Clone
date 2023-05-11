e9c276e473e28447b9d08d52b48b1bfd965c69e4
Pásztor Kristóf <pasztor.kristof.kp@gmail.com>
2023-05-11 11:14:59 +0200

    refactor: Remove test project from solution

--- LibraryExplorer/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using LogView.LogTransformer;
-
-var lt = new LogTransformer
-{
-    LogFilePath = @"D:\2023-05-05_19-48-34.json",
-    SettingsFilePath = "D:\\felev6\\onlab\\Polytopia-Clone\\Polytopia Clone\\GameSettings\\PropertiesSettings.json"
-};
-var json = lt.Transform();
-File.WriteAllText("D:\\test.json", json);
