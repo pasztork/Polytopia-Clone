5f5f9d1c57ce50e849b3a3e0f17feb4d48d89c20
Levi0727 <molnar.levente11@gmail.com>
2023-09-29 19:31:03 +0200

    feat: add docker-client files and Client project

---
+++ DockerClient/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Client;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        await StartContainer("container_name");
+        await StartContainer("container_name");
+    }
+
+    private static async Task StartContainer(string name)
+    {
+        using (DockerClient client = new DockerClientConfiguration().CreateClient())
+        {
+            await client.Containers.StartContainerAsync(name, new ContainerStartParameters());
+        }
+    }
+}
---
+++ example-client/docker_client/src/app.py
@@ -0,0 +1,32 @@
+import json
+import websocket
+import os
+
+import message_handler_v2
+
+SERVER_URL = f'ws://{os.environ.get("SERVER_URL")}:53658/ws'
+NAME = os.environ.get('PLAYER_NAME')
+
+
+def on_open(ws):
+    message = { 'Name': NAME }
+    ws.send(json.dumps(message))
+
+
+def on_message(ws, message):
+    response = message_handler_v2.get_response_for(message)
+    if (response == None):
+        return
+    ws.send(json.dumps(response))
+
+
+def connect_and_listen():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        SERVER_URL, on_open=on_open, on_message=on_message)
+    ws.run_forever()
+
+
+if __name__ == '__main__':
+    message_handler_v2.NAME = NAME
+    connect_and_listen()
