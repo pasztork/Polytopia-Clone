0ce2d2676883633ae94c4529159036494664da87
Levi0727 <molnar.levente11@gmail.com>
2023-10-24 20:07:30 +0200

    feat: add WebServer project
    add WebServer project to Solution
    add Index.razor
    add Upload.razor and file uploading

---
+++ WebServer/Data/Submission.cs
@@ -0,0 +1,18 @@
+﻿namespace WebServer.Data;
+
+using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+public class Submission
+{
+    [Required(ErrorMessage = "Name is required.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(maximumLength: 6, MinimumLength = 6, ErrorMessage = "Neptun code must be exactly 6 characters long.")]
+    public string Neptun { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "File is required")]
+    [FileExtensions(Extensions = ".zip", ErrorMessage = "File extension must be zip.")]
+    public IBrowserFile? File { get; set; }
+}
