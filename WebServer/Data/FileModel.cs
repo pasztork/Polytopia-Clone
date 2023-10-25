d89a0088ecf949cb542f4f9c219314ce45e46de8
Levi0727 <molnar.levente11@gmail.com>
2023-10-25 12:20:35 +0200

    feat: start to make Tournament page
    add chooser list for ai and map files

---
+++ WebServer/Data/FileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServer.Data
+{
+    public class FileModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public bool IsSelected { get; set; } = false;
+    }
+}
